6c2490e6746a527b736d34d50cabb8d2ef62d016
raja.charuvil <raja.charuvil@fundingcircle.com>
2020-01-10 15:32:48 +0000

    Removed some code

--- script.py
+++ script.py
@@ -1,17 +1,7 @@
 import os
-import sys
+
 
 import requests
-
-import math
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_great):
-    greeting = "Hello, {}".format(who_to_great)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
